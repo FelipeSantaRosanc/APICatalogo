0edd2a05658bbead5f64e50115f7ce02b216c8eb
FelipeSantaRosanc <felipe.srncarvalho@gmail.com>
2025-01-07 20:08:15 -0300

    Add project files.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Model;
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext( options )
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+    }
+}
---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using APICatalogo.Models;
+
+
+namespace APICatalogo.Model
+{
+    public class Categoria
+    {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+        public int CategoriaId { get; set; }
+        public string? Nome{ get; set; }
+
+        public string? ImagemUrl{ get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
+
+    }
+}
