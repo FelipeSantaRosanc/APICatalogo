f718e06293dd28a0ffce44819de81685dcbd9c96
FelipeSantaRosanc <felipe.srncarvalho@gmail.com>
2025-01-18 19:05:21 -0300

    Aplicando Validação Personalizada

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -2,16 +2,18 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using APICatalogo.Model;
+using APICatalogo.Validations;
 
 namespace APICatalogo.Models
 {
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
 
         [Required(ErrorMessage = "O nome é obrigatório!")]
         [StringLength(20, ErrorMessage = "O nome deve possuir entre 5 e 20 caracteres", MinimumLength = 5)]
+       // [PrimeiraLetraMaiuscula]
         public string? Nome { get; set; }
 
         [Required]
@@ -36,6 +38,29 @@ namespace APICatalogo.Models
         [JsonIgnore]
         public Categoria? Categoria { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra do produto deve ser maiúscula",
+                    new[]
+                    {
+                        nameof(this.Nome)
+                    });
+                }
+            }
 
-}
+            if (Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero",
+                new[]
+                {
+                    nameof(this.Estoque)
+                });
+            }
+        }
+    }
 }
---
+++ APICatalogo/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Validations
+{
+    public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
